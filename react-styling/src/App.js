c41f8cbc469a8442277c219736dd37519caa2c62
pnichols789 <pnichols789@gmail.com>
2022-10-08 15:18:49 -0500

    css module styling

--- react-styling/src/App.js
+++ react-styling/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Inline from "./components/inline/Inline"
 import Stylesheet from './components/stylesheet';
+import Button from './components/cssModules/index';
 
 function App() {
   return (
     <div className="App">
       <Inline/>
       <Stylesheet/>
+      <Button/>
     </div>
   );
 }
---
+++ react-styling/src/components/cssModules/index.js
@@ -0,0 +1,7 @@
+import styles from './button.module.css';
+
+function Button () {
+    return <button className={styles.default}>Sample Button</button>
+}
+
+export default Button;
