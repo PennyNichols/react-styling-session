08b1db9a2fc2e281ec26e449664bf0546e6d1e6f
pnichols789 <pnichols789@gmail.com>
2022-10-08 14:43:05 -0500

    css stylesheet component

--- react-styling/src/App.js
+++ react-styling/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Inline from "./components/inline/Inline"
+import Stylesheet from './components/stylesheet';
 
 function App() {
   return (
     <div className="App">
       <Inline/>
+      <Stylesheet/>
     </div>
   );
 }
---
+++ react-styling/src/components/stylesheet/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+function Stylesheet () {
+    return <div className='container'>
+        This is a Stylesheet component
+    </div>
+};
+
+export default Stylesheet;
