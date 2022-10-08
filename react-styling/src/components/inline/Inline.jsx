2e0e2c43c338a9d474ebcbe4cf0a98952149a601
pnichols789 <pnichols789@gmail.com>
2022-10-08 14:32:24 -0500

    inline styling imported from separate js styling

--- react-styling/src/components/inline/Inline.jsx
+++ react-styling/src/components/inline/Inline.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {myStyles} from './styles';
+
+
+const styleObj = {
+    color: 'red',
+    backgroundColor: '#e0e0e0',
+    fontWeight: 'lighter',
+};
+
 
 const Inline = () => {
   return (
-    <div style={{color:'purple', backgroundColor:'#e0e0e0', fontWeight:'bold'}}>Inline Styling Component</div>
-  )
+    <>
+        <div style={{color:'purple', backgroundColor:'#e0e0e0', fontWeight:'bold'}}>Inline Styling Component</div>
+        <div style={styleObj}>Inline components with constant javascript object</div>
+        <div style={myStyles.div}>Inline styling with external js stylesheet</div>
+        <div style={myStyles.par}>Inline styling with external js stylesheet</div>
+    </>
+
+
+    )
 }
 
 export default Inline
